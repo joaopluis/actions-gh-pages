ddbdf658961c703a2c2b3133d14bc57cef9a77ea
João Luís <joao.luis@mercedes-benz.io>
2023-11-24 11:45:35 +0000

    Fix known hosts

--- src/set-tokens.ts
+++ src/set-tokens.ts
@@ -20,6 +20,19 @@ export async function setSSHKey(inps: Inputs, publishRepo: string): Promise<stri
   await io.mkdirP(sshDir);
   await exec.exec('chmod', ['700', sshDir]);
 
+  const knownHosts = path.join(sshDir, 'known_hosts');
+  // ssh-keyscan -t rsa github.com or serverUrl >> ~/.ssh/known_hosts on Ubuntu
+  const cmdSSHkeyscanOutput = `\
+# ${getServerUrl().host}:22 SSH-2.0-babeld-1f0633a6
+${
+  getServerUrl().host
+} ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCj7ndNxQowgcQnjshcLrqPEiiphnt+VTTvDP6mHBL9j1aNUkY4Ue1gvwnGLVlOhGeYrnZaMgRK6+PKCUXaDbC7qtbW8gIkhL7aGCsOr/C56SJMy/BCZfxd1nWzAOxSDPgVsmerOBYfNqltV9/hWCqBywINIR+5dIg6JTJ72pcEpEjcYgXkE2YEFXV1JHnsKgbLWNlhScqb2UmyRkQyytRLtL+38TGxkxCflmO+5Z8CSSNY7GidjMIZ7Q4zMjA2n1nGrlTDkzwDCsw+wqFPGQA179cnfGWOWRVruj16z6XyvxvjJwbz0wQZ75XK5tKSb7FNyeIEs4TT4jk+S4dhPeAUC5y+bDYirYgM4GC7uEnztnZyaVWQ7B381AK4Qdrwt51ZqExKbQpTUNn+EjqoTwvqNj4kqx5QUCI0ThS/YkOxJCXmPUWZbhjpCg56i+2aB6CmK2JGhn57K5mj0MNdBXA4/WnwH6XoPWJzK5Nyu2zB3nAZp+S5hpQs+p1vN1/wsjk=
+`;
+  fs.writeFileSync(knownHosts, cmdSSHkeyscanOutput + '\n');
+  await exec.exec('ssh-keyscan', ['-t', 'rsa', getServerUrl().host, '>>', '~/.ssh/known_hosts']);
+  core.info(`[INFO] wrote ${knownHosts}`);
+  await exec.exec('chmod', ['600', knownHosts]);
+
   const idRSA = path.join(sshDir, 'github');
   fs.writeFileSync(idRSA, inps.DeployKey + '\n');
   core.info(`[INFO] wrote ${idRSA}`);
